b8eda0ab3ee8cc424c05d5e98fe4e2d4d5d7c6b7
Rinat Valiullov <4rontender@gmail.com>
2021-07-29 17:47:49 +0500

    keep going to understand logic + add some dependencies

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,7 +1,22 @@
 "use strict";
 var applyButton = document.querySelector('.apply-filter');
 var inputDateFilter = document.querySelector('#date-filter');
-applyButton.addEventListener('click', function (e) {
-    var t1 = document.querySelectorAll('.event-characteristics time')[0];
-    console.log(parseInt(inputDateFilter.value.substr(8, 2), 10) === parseInt(t1.dateTime.substr(0, 2), 10));
+// applyButton.addEventListener('click', e => {
+//   let t1 = document.querySelectorAll('.event-characteristics time')[0] as HTMLTimeElement;
+//   console.log(parseInt(inputDateFilter.value.substr(8, 2), 10) === parseInt(t1.dateTime.substr(0, 2), 10));
+// });
+var events = document.querySelectorAll('.event');
+var typeFilterWrapper = document.querySelector('.type-filter-wrapper');
+typeFilterWrapper.addEventListener('click', function (event) {
+    var etarget = event.target;
+    if (etarget.tagName !== 'INPUT') {
+        return false;
+    }
+    events.forEach(function (element) {
+        var typeEvent = element.querySelector('.event-type');
+        var filterType = "" + etarget.name.substr(0, 1).toUpperCase() + (etarget === null || etarget === void 0 ? void 0 : etarget.name.substr(1));
+        if (filterType === typeEvent.innerText) {
+            console.log('+');
+        }
+    });
 });
--- scripts/ts/app.ts
+++ scripts/ts/app.ts
@@ -1,10 +1,31 @@
 const applyButton = document.querySelector('.apply-filter') as HTMLButtonElement;
 const inputDateFilter = document.querySelector('#date-filter') as HTMLInputElement;
 
-applyButton.addEventListener('click', e => {
+// applyButton.addEventListener('click', e => {
 
-  let t1 = document.querySelectorAll('.event-characteristics time')[0] as HTMLTimeElement;
+//   let t1 = document.querySelectorAll('.event-characteristics time')[0] as HTMLTimeElement;
 
-  console.log(parseInt(inputDateFilter.value.substr(8, 2), 10) === parseInt(t1.dateTime.substr(0, 2), 10));
+//   console.log(parseInt(inputDateFilter.value.substr(8, 2), 10) === parseInt(t1.dateTime.substr(0, 2), 10));
 
-});
+// });
+
+
+const events = document.querySelectorAll('.event') as NodeList;
+const typeFilterWrapper = document.querySelector('.type-filter-wrapper') as HTMLElement;
+
+typeFilterWrapper.addEventListener('click', (event) => {
+  const etarget = event.target as HTMLInputElement;
+  if (etarget.tagName !== 'INPUT') {
+    return false;
+  }
+
+  events.forEach((element) => {
+    let typeEvent = (element as HTMLDivElement).querySelector('.event-type');
+    const filterType = `${etarget.name.substr(0, 1).toUpperCase()}${etarget?.name.substr(1)}`;
+    if (filterType === (typeEvent as HTMLDivElement).innerText) {
+      console.log('+')
+    }
+
+  })
+
+})
