1c8282536ed7ec96659d25a88fb27e76637086c2
Rinat Valiullov <4rontender@gmail.com>
2021-07-30 04:10:49 +0500

    feat: add basic logic of type event filter

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,22 +1,46 @@
 "use strict";
-var applyButton = document.querySelector('.apply-filter');
-var inputDateFilter = document.querySelector('#date-filter');
+// const applyButton = document.querySelector('.apply-filter') as HTMLButtonElement;
+// const inputDateFilter = document.querySelector('#date-filter') as HTMLInputElement;
 // applyButton.addEventListener('click', e => {
 //   let t1 = document.querySelectorAll('.event-characteristics time')[0] as HTMLTimeElement;
 //   console.log(parseInt(inputDateFilter.value.substr(8, 2), 10) === parseInt(t1.dateTime.substr(0, 2), 10));
 // });
-var events = document.querySelectorAll('.event');
-var typeFilterWrapper = document.querySelector('.type-filter-wrapper');
-typeFilterWrapper.addEventListener('click', function (event) {
-    var etarget = event.target;
-    if (etarget.tagName !== 'INPUT') {
-        return false;
-    }
-    events.forEach(function (element) {
-        var typeEvent = element.querySelector('.event-type');
-        var filterType = "" + etarget.name.substr(0, 1).toUpperCase() + (etarget === null || etarget === void 0 ? void 0 : etarget.name.substr(1));
-        if (filterType === typeEvent.innerText) {
-            console.log('+');
-        }
+const events = document.querySelectorAll('.event');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const typeFilterWrapper = document.querySelector('.type-filter-wrapper');
+// typeFilterWrapper.addEventListener('click', (event) => {
+//   const etarget = event.target as HTMLInputElement;
+//   if (etarget.tagName !== 'INPUT') {
+//     return false;
+//   }
+//   events.forEach((element) => {
+//     let typeEvent = (element as HTMLDivElement).querySelector('.event-type');
+//     const filterType = `${etarget.name.substr(0, 1).toUpperCase()}${etarget?.name.substr(1)}`;
+//     if (filterType === (typeEvent as HTMLDivElement).innerText) {
+//       (typeEvent as HTMLDivElement).innerHTML = ''
+//     }
+//     // if (etarget.checked) {
+//     //   (typeEvent as HTMLDivElement).innerHTML = ''
+//     // }
+//   })
+// })
+document.addEventListener('click', event => {
+    checkboxes.forEach((checkbox, index) => {
+        events.forEach((eventBlock, index) => {
+            var _a, _b;
+            let typeEvent = eventBlock.querySelector('.event-type');
+            let checkboxValue = checkbox.name;
+            // eventBlock.classList.remove('hide');
+            if (checkbox.checked) {
+                if (checkboxValue === ((_a = typeEvent === null || typeEvent === void 0 ? void 0 : typeEvent.textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase())) {
+                    eventBlock.classList.remove('hide');
+                }
+            }
+            if (!checkbox.checked) {
+                if (checkboxValue === ((_b = typeEvent === null || typeEvent === void 0 ? void 0 : typeEvent.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase())) {
+                    eventBlock.classList.add('hide');
+                }
+            }
+        });
     });
 });
--- scripts/ts/app.ts
+++ scripts/ts/app.ts
@@ -1,5 +1,5 @@
-const applyButton = document.querySelector('.apply-filter') as HTMLButtonElement;
-const inputDateFilter = document.querySelector('#date-filter') as HTMLInputElement;
+// const applyButton = document.querySelector('.apply-filter') as HTMLButtonElement;
+// const inputDateFilter = document.querySelector('#date-filter') as HTMLInputElement;
 
 // applyButton.addEventListener('click', e => {
 
@@ -11,21 +11,45 @@ const inputDateFilter = document.querySelector('#date-filter') as HTMLInputEleme
 
 
 const events = document.querySelectorAll('.event') as NodeList;
+const checkboxes = document.querySelectorAll('input[type="checkbox"]') as NodeList;
 const typeFilterWrapper = document.querySelector('.type-filter-wrapper') as HTMLElement;
 
-typeFilterWrapper.addEventListener('click', (event) => {
-  const etarget = event.target as HTMLInputElement;
-  if (etarget.tagName !== 'INPUT') {
-    return false;
-  }
+// typeFilterWrapper.addEventListener('click', (event) => {
+//   const etarget = event.target as HTMLInputElement;
+//   if (etarget.tagName !== 'INPUT') {
+//     return false;
+//   }
 
-  events.forEach((element) => {
-    let typeEvent = (element as HTMLDivElement).querySelector('.event-type');
-    const filterType = `${etarget.name.substr(0, 1).toUpperCase()}${etarget?.name.substr(1)}`;
-    if (filterType === (typeEvent as HTMLDivElement).innerText) {
-      console.log('+')
-    }
+//   events.forEach((element) => {
+//     let typeEvent = (element as HTMLDivElement).querySelector('.event-type');
+//     const filterType = `${etarget.name.substr(0, 1).toUpperCase()}${etarget?.name.substr(1)}`;
+//     if (filterType === (typeEvent as HTMLDivElement).innerText) {
+//       (typeEvent as HTMLDivElement).innerHTML = ''
+//     }
+//     // if (etarget.checked) {
+//     //   (typeEvent as HTMLDivElement).innerHTML = ''
+//     // }
 
-  })
+//   })
 
+// })
+
+document.addEventListener('click', event => {
+  checkboxes.forEach((checkbox, index) => {
+    events.forEach((eventBlock, index) => {
+      let typeEvent = (eventBlock as HTMLDivElement).querySelector('.event-type');
+      let checkboxValue = (checkbox as HTMLInputElement).name;
+      // eventBlock.classList.remove('hide');
+      if ((checkbox as HTMLInputElement).checked) {
+        if (checkboxValue === typeEvent?.textContent?.toLowerCase()) {
+          (eventBlock as HTMLDivElement).classList.remove('hide');
+        }
+      }
+      if (!(checkbox as HTMLInputElement).checked) {
+        if (checkboxValue === typeEvent?.textContent?.toLowerCase()) {
+          (eventBlock as HTMLDivElement).classList.add('hide');
+        }
+      }
+    });
+  });
 })
